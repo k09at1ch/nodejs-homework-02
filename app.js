b7b71719d8ac846c9bdaa00169cffea2c604095f
k09at1ch <117813113+k09at1ch@users.noreply.github.com>
2023-08-22 21:43:07 +0300

    small redo

--- app.js
+++ app.js
@@ -21,5 +21,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 app.listen(3000, () => {
-  console.log(`Server is running on port ${3000}`);
+  console.log(`Server is running on port 3000`);
 });
