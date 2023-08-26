b074d2322b50c83de0e030960e4b1815a05203de
k09at1ch <117813113+k09at1ch@users.noreply.github.com>
2023-08-26 15:20:44 +0300

    red0

--- app.js
+++ app.js
@@ -1,106 +1,14 @@
 const express = require("express");
-const router = express.Router();
-const { v4: uuidv4 } = require('uuid');
 const morgan = require('morgan');
 const cors = require('cors');
-const fs = require('fs/promises');
+const contactsRouter = require('./routes/api/contacts');
 
-const newId = uuidv4();
-
-const contactsRouter = express.Router();
 const app = express();
-
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(morgan(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
-
-let contacts = [];
-fs.readFile('contacts.json', 'utf8')
-  .then((data) => {
-    contacts = JSON.parse(data);
-  })
-  .catch((err) => {
-    console.error('Error reading contacts file:', err);
-  });
-
-contactsRouter.get("/", (req, res, next) => {
-  res.status(200).json(contacts);
-});
-
-contactsRouter.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const contact = contacts.find((el) => el.id === id);
-  if (contact) {
-    res.status(200).json(contact);
-  } else {
-    res.status(404).json({ message: 'Contact not found' });
-  }
-});
-
-contactsRouter.post("/", (req, res, next) => {
-  const { name, phone, email } = req.body;
-  const contact = {
-    name,
-    phone,
-    email,
-    id: uuidv4(),
-  };
-
-  contacts.push(contact);
-
-  fs.writeFile('contacts.json', JSON.stringify(contacts, null, 2), 'utf8')
-    .then(() => {
-      res.status(200).json(contact);
-    })
-    .catch((err) => {
-      console.error('Error writing to contacts file:', err);
-      res.status(500).json({ message: 'Internal Server Error' });
-    });
-});
-
-contactsRouter.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  const index = contacts.findIndex((el) => el.id === id);
-  if (index !== -1) {
-    contacts.splice(index, 1);
-
-    fs.writeFile('contacts.json', JSON.stringify(contacts, null, 2), 'utf8')
-      .then(() => {
-        res.status(204).json();
-      })
-      .catch((err) => {
-        console.error('Error writing to contacts file:', err);
-        res.status(500).json({ message: 'Internal Server Error' });
-      });
-  } else {
-    res.status(404).json({ message: 'Contact not found' });
-  }
-});
-
-contactsRouter.put("/:id", (req, res, next) => {
-  const { id } = req.params;
-  const { phone, name, email } = req.body;
-  const index = contacts.findIndex((el) => el.id === id);
-
-  if (index !== -1) {
-    contacts[index].phone = phone;
-    contacts[index].name = name;
-    contacts[index].email = email;
-    fs.writeFile('contacts.json', JSON.stringify(contacts, null, 2), 'utf8')
-      .then(() => {
-        res.status(200).json(contacts[index]);
-      })
-      .catch((err) => {
-        console.error('Error writing to contacts file:', err);
-        res.status(500).json({ message: 'Internal Server Error' });
-      });
-  } else {
-    res.status(404).json({ message: 'Contact not found' });
-  }
-});
 
 app.use('/api/contacts', contactsRouter);
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,104 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs/promises");
 
-const router = express.Router()
+const newId = uuidv4();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+let contacts = [];
+fs.readFile("contacts.json", "utf8")
+  .then((data) => {
+    contacts = JSON.parse(data);
+  })
+  .catch((err) => {
+    console.error("Error reading contacts file:", err);
+  });
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", (req, res, next) => {
+  res.status(200).json(contacts);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const contact = contacts.find((el) => el.id === id);
+  if (contact) {
+    res.status(200).json(contact);
+  } else {
+    res.status(404).json({ message: "Contact not found" });
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", (req, res) => {
+  const { name, phone, email } = req.body;
+  console.log(req.body);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (name === undefined || phone === undefined || email === undefined) {
+    console.log("Invalid input");
+    return res.status(400).json({ message: "Invalid input" });
+  }
 
-module.exports = router
+  const contact = {
+    name,
+    phone,
+    email,
+    id: uuidv4(),
+  };
+  contacts.push(contact);
+  console.table(contacts);
+
+  fs.writeFile(
+    "contacts.json",
+    JSON.stringify(contacts, null, 2),
+    "utf8",
+    (err) => {
+      if (err) {
+        console.error("Error writing to contacts file:", err);
+        return res.status(500).json({ message: "Internal Server Error" });
+      }
+      res.status(200).json(contact);
+    }
+  );
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const index = contacts.findIndex((el) => el.id === id);
+  if (index !== -1) {
+    contacts.splice(index, 1);
+
+    fs.writeFile("contacts.json", JSON.stringify(contacts, null, 2), "utf8")
+      .then(() => {
+        res.status(204).json();
+      })
+      .catch((err) => {
+        console.error("Error writing to contacts file:", err);
+        res.status(500).json({ message: "Internal Server Error" });
+      });
+  } else {
+    res.status(404).json({ message: "Contact not found" });
+  }
+});
+
+router.put("/:id", (req, res, next) => {
+  const { id } = req.params;
+  const { phone, name, email } = req.body;
+  const index = contacts.findIndex((el) => el.id === id);
+
+  if (index !== -1) {
+    contacts[index].phone = phone;
+    contacts[index].name = name;
+    contacts[index].email = email;
+    fs.writeFile("contacts.json", JSON.stringify(contacts, null, 2), "utf8")
+      .then(() => {
+        res.status(200).json(contacts[index]);
+      })
+      .catch((err) => {
+        console.error("Error writing to contacts file:", err);
+        res.status(500).json({ message: "Internal Server Error" });
+      });
+  } else {
+    res.status(404).json({ message: "Contact not found" });
+  }
+});
+
+module.exports = router;
